38af0b7a30a69917ac1352fd53413894e90fb501
DOAgustavo <givoannigustavos@gmail.com>
2025-04-01 05:34:49 -0300

    finalizando

--- lib/main.dart
+++ lib/main.dart
@@ -92,12 +92,4 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class MaintenanceLogPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Registro de Manutenções')),
-      body: Center(child: Text('Página para registrar manutenções.')),
-    );
-  }
-}
+
